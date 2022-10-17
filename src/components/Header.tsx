1e239672a5d8f5fa562f713fc20ebcabfb218baa
keven william silva barros <69265078+kevenbarros@users.noreply.github.com>
2022-10-17 09:23:44 -0300

    typescript

---
+++ src/components/Header.tsx
@@ -0,0 +1,154 @@
+import { Text, StyleSheet, View } from '@react-pdf/renderer';
+import LogoText from "../svg/LogoText"
+import Arrow from "../svg/Arrow"
+import React, { FC } from 'react';
+import { PDFProps } from '../types/DataProps';
+
+
+const month = [
+  "JAN",
+  "FEV",
+  "MAR",
+  "ABR",
+  "MAI",
+  "JUN",
+  "JUL",
+  "AGO",
+  "SET",
+  "OUT",
+  "NOV",
+  "DEZ"
+]
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#1C2025",
+    paddingHorizontal: 124,
+    paddingVertical: 32,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 60,
+    height: 24,
+  },
+  titleWithe: {
+    marginTop: 10,
+    fontSize: 22,
+    color: "#EEF1F2",
+    letterSpacing: 0.64,
+    fontFamily: "Nunito",
+    fontWeight: 600,
+  },
+  titleGold: {
+    marginTop: 8,
+    fontWeight: 400,
+    fontSize: 24,
+    color: "#FACA50",
+    letterSpacing: 0.64,
+    fontFamily: "Nunito",
+  },
+  extractMovimentsBox: {
+    marginTop: 32,
+    display: "flex",
+    flexDirection: "column",
+    width: 150,
+  },
+  extractMoviments: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 4,
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  movimentsContainerTitles: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  movimentsTitles: {
+    fontSize: 8,
+    color: "#7F8C9A",
+    fontWeight: 400,
+    fontFamily: "Open Sans"
+  },
+  movimentsValue: {
+    fontSize: 8,
+    color: "#B5BFC9",
+    fontWeight: 400,
+    fontFamily: "Open Sans"
+  },
+  titleDate: {
+    color: "#B5BFC9",
+    fontSize: 8,
+    fontWeight: 400,
+    fontFamily: "Open Sans"
+  },
+  imageArrow: {
+    width: 14,
+    height: 14,
+  },
+})
+
+const getFullDate = ({ date }: any) => {
+  const newDt = new Date(date)
+  let day = newDt.getDate().toString()
+  day = (day.length === 1) ? '0' + day : day;
+  const aux = `${day} ${month[newDt.getMonth()]} DE ${newDt.getFullYear()}`
+  return aux
+}
+
+const getDayMonth = (date: any) => {
+  const newDt = new Date(date)
+  let day = newDt.getDate().toString()
+  day = (day.length === 1) ? '0' + day : day;
+  const aux = `${day} ${month[newDt.getMonth()]}`
+  return aux
+}
+
+const maskMoney = (value: Number) => {
+  return value.toLocaleString('pt-br', { minimumFractionDigits: 2 });
+}
+
+const Header: FC<PDFProps> = ({ data }) => {
+  return <View style={styles.header}>
+    <LogoText></LogoText>
+    <Text style={styles.titleWithe}>
+      Este é o seu balanço geral na Hurst Wallet:
+    </Text>
+
+    <View style={styles.extractMovimentsBox}>
+      <Text style={styles.titleDate}>{`${getDayMonth(data.initialExtractDate)} A ${getFullDate(data.endExtractDate)}`}</Text>
+      <View style={styles.extractMoviments}>
+        <View style={styles.movimentsContainerTitles}>
+          <Arrow></Arrow>
+          <Text style={styles.movimentsTitles}>Entradas</Text>
+        </View>
+        <Text style={styles.movimentsValue}>+ R${maskMoney(data.walletEntries)}</Text>
+      </View>
+      <View style={styles.extractMoviments}>
+        <View style={styles.movimentsContainerTitles}>
+          <Arrow></Arrow>
+          <Text style={styles.movimentsTitles}>Saídas</Text>
+        </View>
+        <Text style={styles.movimentsValue}>- R${maskMoney(data.walletOutputs)}</Text>
+      </View>
+      <View style={styles.extractMoviments}>
+        <View style={styles.movimentsContainerTitles}>
+          <Arrow></Arrow>
+          <Text style={styles.movimentsTitles}>Totais</Text>
+        </View>
+        <Text style={styles.movimentsValue}>+ R${maskMoney(data.walletTotals)}</Text>
+      </View>
+      <View style={styles.extractMoviments}>
+        <View style={styles.movimentsContainerTitles}>
+          <Arrow></Arrow>
+          <Text style={styles.movimentsTitles}>Movimentações</Text>
+        </View>
+        <Text style={styles.movimentsValue}>+ R${maskMoney(data.walletMovements)}</Text>
+      </View>
+    </View>
+  </View>
+}
+export default Header;
---
+++ src/components/Table.tsx
@@ -0,0 +1,230 @@
+import { Text, StyleSheet, View } from '@react-pdf/renderer';
+import Logo from "../svg/Logo"
+// import { typeImg, getDayMonth, getFullDate } from "../helpers/common"
+import React, { FC } from 'react';
+import { PDFProps } from '../types/DataProps';
+
+const month = [
+  "JAN",
+  "FEV",
+  "MAR",
+  "ABR",
+  "MAI",
+  "JUN",
+  "JUL",
+  "AGO",
+  "SET",
+  "OUT",
+  "NOV",
+  "DEZ"
+]
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#F7F7F7",
+    paddingHorizontal: 32,
+    paddingVertical: 24,
+    display: "flex",
+    flexDirection: "column",
+  },
+  bodyHeader: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+
+  headerLogo: {
+    width: 21.2,
+    height: 24,
+  },
+  headerInformations: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-end"
+  },
+  headerInformationsName: {
+    fontSize: 8,
+    color: "#2E343A",
+    fontWeight: 700,
+    fontFamily: "Nunito",
+  },
+  headerInformationsPdf: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 8,
+  },
+  headerInformationsEmission: {
+    display: "flex",
+    flexDirection: "row",
+    marginLeft: 8,
+  },
+  headerInformationsEmissionValue: {
+    color: "#7F8C9A",
+    fontSize: 8,
+    marginLeft: 4,
+    fontFamily: "Nunito",
+    fontWeight: 400
+  },
+  table: {
+    marginTop: 32,
+    display: "flex",
+    flexDirection: "column"
+  },
+  tableHeader: {
+    paddingVertical: 6,
+    display: "flex",
+    flexDirection: "column",
+
+  },
+  titlesContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottom: "1px solid #D3D8DF",
+    marginBottom: 16,
+    paddingBottom: 8,
+  },
+  titles: {
+    fontSize: 8,
+    color: "#667382",
+    fontFamily: "Nunito",
+    fontWeight: 400
+  },
+  titlesImportant: {
+    fontSize: 12,
+    color: "#232930",
+    fontWeight: 700,
+    display: "flex",
+    justifyContent: "center",
+    paddingRight: 50,
+    fontFamily: "Nunito",
+
+  },
+  balance: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end"
+  },
+  balanceTitle: {
+    fontSize: 8,
+    fontWeight: 700,
+    color: "#2E343A",
+    fontFamily: "Nunito",
+  },
+  balanceValue: {
+    fontSize: 8,
+    fontWeight: 700,
+    marginLeft: 4,
+    color: "#7F8C9A",
+    fontFamily: "Nunito",
+  },
+  itemContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 11,
+    paddingRight: 6
+  },
+  itemMovements: {
+    fontSize: 10,
+    fontWeight: 800,
+    color: "#2E343A",
+    width: "100%",
+    paddingHorizontal: 16,
+    alignSelf: "flex-start",
+    paddingTop: 2,
+    fontFamily: "Nunito",
+  },
+  itemValue: {
+    fontSize: 8,
+    fontWeight: 700,
+    color: "#2E343A",
+    fontFamily: "Nunito",
+  },
+
+  dateContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  typeLogo: {
+    marginLeft: 15
+  },
+  tableBody: {
+    display: "flex",
+    flexDirection: "column",
+  }
+
+})
+
+const getFullDate = ({ date }: any) => {
+  const newDt = new Date(date)
+  let day = newDt.getDate().toString()
+  day = (day.length === 1) ? '0' + day : day;
+  const aux = `${day} ${month[newDt.getMonth()]} DE ${newDt.getFullYear()}`
+  return aux
+}
+
+const getDayMonth = (date: any) => {
+  const newDt = new Date(date)
+  let day = newDt.getDate().toString()
+  day = (day.length === 1) ? '0' + day : day;
+  const aux = `${day} ${month[newDt.getMonth()]}`
+  return aux
+}
+
+const maskMoney = (value: Number) => {
+  return value.toLocaleString('pt-br', { minimumFractionDigits: 2 });
+}
+
+const Table: FC<PDFProps> = ({ data }) => {
+  return <View style={styles.container}>
+    <View style={styles.bodyHeader}>
+      <Logo></Logo>
+      <View style={styles.headerInformations}>
+        <Text style={styles.headerInformationsName}>{data.userName}</Text>
+        <View style={styles.headerInformationsPdf}>
+          <View style={styles.headerInformationsEmission}>
+            <Text style={styles.headerInformationsName}>EXTRATO WALLET</Text>
+            <Text style={styles.headerInformationsEmissionValue}>{`${getDayMonth(data.initialExtractDate)} A ${getFullDate(data.endExtractDate)}`}</Text>
+          </View>
+          <View style={styles.headerInformationsEmission}>
+            <Text style={styles.headerInformationsName}>EMISSÃO</Text>
+            <Text style={styles.headerInformationsEmissionValue}>{getFullDate(data.issueDate)}</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+    <View style={styles.table}>
+      <View style={styles.tableHeader}>
+        <View style={styles.titlesContainer}>
+          <Text style={styles.titles}>{`${getDayMonth(data.initialExtractDate)} A ${getFullDate(data.endExtractDate)}`}</Text>
+          <Text style={styles.titlesImportant}>Movimentações</Text>
+          <Text style={styles.titles}>VALOR EM R$</Text>
+        </View>
+        <View style={styles.balance}>
+          <Text style={styles.balanceTitle}>SALDO NESTE PERÍODO:</Text>
+          <Text style={styles.balanceValue}>R$ {maskMoney(data.balancePeriodical)}</Text>
+        </View>
+      </View>
+      <View style={styles.tableBody}>
+        {data.movements.map((elemento, index) => {
+          return <View style={styles.itemContainer} key={index}>
+            <View style={styles.dateContainer}>
+              <Text style={styles.itemValue}>{getDayMonth(elemento.date)}</Text>
+              <View style={styles.typeLogo}>
+                {typeImg(elemento.type)}
+              </View>
+            </View>
+            <Text style={styles.itemMovements}>{elemento.description}</Text>
+            <Text style={styles.itemValue}>R$ {maskMoney(elemento.value)}</Text>
+          </View>
+        })}
+      </View>
+    </View>
+  </View>
+}
+export default Table;
